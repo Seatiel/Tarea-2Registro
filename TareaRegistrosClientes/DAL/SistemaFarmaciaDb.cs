9d9f2f75e29aeca97d91b725f82a198f588eb052
seatiel ygnacio rosario <seatiel_2010@hotmail.com>
2017-05-14 11:45:18 -0400

    Tarea de registro

---
+++ TareaRegistrosClientes/DAL/SistemaFarmaciaDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TareaRegistrosClientes.Models;
+
+namespace TareaRegistrosClientes.DAL
+{
+    public class SistemaFarmaciaDb : DbContext 
+    {
+        public SistemaFarmaciaDb() : base("ConStr")
+        {
+                
+        }
+
+        public DbSet<Clientes> Cliente { get; set; }
+    }
+
+}
---
+++ TareaRegistrosClientes/Models/Clientes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TareaRegistrosClientes.Models
+{
+    public class Clientes
+    {
+        [Key]
+
+        public int ClienteId { get; set; }
+        //[DataType(DataType.Text)]
+        [StringLength(100)]
+        [RegularExpression("^([a-zA-Z .&']+)$", ErrorMessage = "Solo letras")]
+        [Required(ErrorMessage = "Debe ingresar el Nombre y Apellido.")]
+        public string Nombres { get; set; }
+
+        //[DataType(DataType.Text)]
+        [StringLength(100)]
+        [RegularExpression("^([a-zA-Z0-9 .&'-.&'#]+)$", ErrorMessage = "Caracteres invalidos")]
+        [Required(ErrorMessage = "Debe ingresar la Direccion.")]
+        public string Direccion { get; set; }
+
+        //[DataType(DataType.PhoneNumber)]
+        [StringLength(100)]
+        [RegularExpression("^([0-9.&'-]+)$", ErrorMessage = "Solo Numeros y (-)")]
+        [Required(ErrorMessage = "Debe ingresar el Telefono.")]
+        public string Telefono { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime FechaCreacion { get; set; }
+
+        //[DataType(DataType.EmailAddress)]
+        [StringLength(100)]
+        [RegularExpression("^([a-zA-Z0-9.&'_.&'@]+)$", ErrorMessage = "Caracteres invalidos")]
+        [Required(ErrorMessage = "Debe ingresar ingresar el Email.")]
+        public string Email { get; set; }
+
+    }
+}
